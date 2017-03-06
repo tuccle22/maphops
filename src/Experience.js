e4dd27eae8c73ce853720ee70e574e9c16d347af
tuccle22 <tuccle22@gmail.com>
2017-03-06 01:47:36 -0500

    Now Resume :)

---
+++ src/Experience.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import './App.css';
+
+export default class Experience extends React.Component {
+
+  render() {
+
+    return (
+        <div className="w3-container w3-card-2 w3-white w3-margin-bottom">
+          <h2 className="w3-text-grey w3-padding-16">
+            <i className="w3-margin-right material-icons w3-xxlarge w3-cell-middle w3-text-teal">business_center</i>
+            Work Experience
+          </h2>
+          <div className="w3-container">
+            <h5 className="w3-opacity">
+              <b>Ping4 Inc | Nashua, NH</b>
+            </h5>
+            <h6 className="w3-text-teal">
+              <i className="w3-margin-right material-icons w3-cell-middle">date_range</i>
+              June 2016 - &nbsp;
+              <span className="w3-tag w3-teal w3-round">Current</span>
+            </h6>
+            <p>
+              Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in
+              deserunt aspernatur est reprehenderit sunt hic. Nulla tempora
+              soluta ea et odio, unde doloremque repellendus iure, iste.
+            </p>
+            <hr/>
+          </div>
+          <div className="w3-container">
+            <h5 className="w3-opacity">
+              <b>UNH Manchester | Manchester, NH</b>
+            </h5>
+            <h6 className="w3-text-teal">
+              <i className="w3-margin-right material-icons w3-cell-middle">date_range</i>
+              September 2015 - May 2016
+            </h6>
+            <p>
+              Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in
+              deserunt aspernatur est reprehenderit sunt hic. Nulla tempora
+              soluta ea et odio, unde doloremque repellendus iure, iste.
+            </p>
+            <hr/>
+          </div>
+          <div className="w3-container">
+            <h5 className="w3-opacity">
+              <b>Single Digits | Bedford, NH</b>
+            </h5>
+            <h6 className="w3-text-teal">
+              <i className="w3-margin-right material-icons w3-cell-middle">date_range</i>
+              February 2014 - April 2015
+            </h6>
+            <p>
+              Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in
+              deserunt aspernatur est reprehenderit sunt hic. Nulla tempora
+              soluta ea et odio, unde doloremque repellendus iure, iste.
+            </p>
+            <hr/>
+          </div>
+          <div className="w3-container">
+            <h5 className="w3-opacity">
+              <b>FairPoint Communications | Manchester, NH</b>
+            </h5>
+            <h6 className="w3-text-teal">
+              <i className="w3-margin-right material-icons w3-cell-middle">date_range</i>
+              January 2012 - January 2013
+            </h6>
+            <p>
+              Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in
+              deserunt aspernatur est reprehenderit sunt hic. Nulla tempora
+              soluta ea et odio, unde doloremque repellendus iure, iste.
+            </p>
+            <br/>
+          </div>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Resume from './Resume';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Resume />,
   document.getElementById('root')
 );
